e3a180e9ca6f80043171a8f51489f9706be349c2
Poopja <poojaparthiban216@gmail.com>
2026-03-11 11:50:55 +0530

    replace all localhost with render url

--- server.js
+++ server.js
@@ -13,14 +13,14 @@ app.use(cors({
 app.use(express.json());
 
 const db = mysql.createPool({
-    host:               process.env.DB_HOST,
-    user:               process.env.DB_USER,
-    password:           process.env.DB_PASSWORD,
-    database:           process.env.DB_NAME,
-    port:               process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit:    10,
-    ssl:                { rejectUnauthorized: false }
+  host:               process.env.DB_HOST,
+  user:               process.env.DB_USER,
+  password:           process.env.DB_PASSWORD,
+  database:           process.env.DB_NAME,
+  port:               process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit:    10,
+  ssl:                { rejectUnauthorized: false }
 });
 
 db.getConnection((err, connection) => {
